1024131d819136171eb6b4da215f36e5939129b1
j <j@mailb.org>
2012-04-09 14:59:18 +0300

    checkout from git

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CH="chroot $1"
-$CH svn checkout http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3 /srv/mediawiki
+$CH git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git /srv/mediawiki
 for ext in TimedMediaHandler TitleBlacklist UploadWizard MwEmbedSupport; do
-$CH svn checkout http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/$ext /srv/mediawiki/extensions/$ext
+$CH git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/$ext.git /srv/mediawiki/extensions/$ext
 done
